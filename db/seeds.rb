e1615bb22334b7d11b36e5fbf9d4382575e6bd2b
rizzi <rizzi@rizzitherm.com.br>
2015-06-30 16:04:43 -0300

    adicionando master user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+#user = CreateAdminService.new.call
+#puts 'CREATED ADMIN USER: ' << user.email
+
+users  = User.create([{ email: 'user@example.com', encrypeted_password: '$2a$10$kf8pAFpLgCJWXWV06J2dGuJ3hE2JG.CeUmHuk6YJmmswQTzzXN5qy', sign_in_count: 2 }])
